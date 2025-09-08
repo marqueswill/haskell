92b17d1e5328ed52b1ed2731880ebaef709d66b5
willyan <willyanmarquesmelo@gmail.com>
2025-09-08 14:44:15 -0300

    LF2 interpreter working New RContext with Decl

--- trabalho1/LF2-Enunciado/Interpreter.hs
+++ trabalho1/LF2-Enunciado/Interpreter.hs
@@ -6,14 +6,20 @@ import Prelude hiding (lookup)
 {- TODO: Estude a definição do tipo Function no arquivo AbsLF.hs e complete as definicoes 
     de "getParams" e "getExp" abaixo. Note "getName" já é fornecida.         
 -}
-getName :: Function -> Ident
-getName (Fun name _ _) = name
+getType :: Function -> Type
+getType (Fun tp name params exp) = tp
 
-getParams :: Function -> [Ident]
-getParams (Fun _ params _) = params
+getDecl :: Function -> Decl -- Gambiarra pra conseguir colocar a função no novo contexto de execução
+getDecl (Fun tp name params exp) =  Dec tp name
+
+getName :: Function -> Ident
+getName (Fun tp name params exp) =  name
+
+getParams :: Function -> [Decl]
+getParams (Fun tp name params exp) =  params
 
 getExp :: Function -> Exp
-getExp (Fun _ _ exp) = exp
+getExp (Fun tp name params exp) =  exp
 
 
 {- : *Não* altere a definição de "executeP" abaixo. 
@@ -21,10 +27,10 @@ getExp (Fun _ _ exp) = exp
          Garanta que saiba explicar verbalmente isso.
 -}
 
+-- Inicializa o contexto de execução com as funções e avalia a expressão do main
 executeP :: Program -> Valor
-
-executeP (Prog fs) =  eval (updatecF [] fs) (expMain fs)
-    where expMain (f:xs)
+executeP (Prog fs) =  eval (updatecF [] fs) (expMain fs) 
+    where expMain (f:xs) -- Procura a função main na lista de funções
               | getName f == Ident "main" =  getExp f
               | otherwise = expMain xs
 
@@ -43,7 +49,7 @@ eval context x = case x of
     ETrue          -> ValorBool True
     EFalse         -> ValorBool False
     EInt n         -> ValorInt n
-    EVar id        -> lookup context  id
+    EVar id        -> lookup context id -- Vai dar ruim?
 {- TODO: remova "undefined" e implemente a avaliação do "EIf" abaixo. A primeira expressao ("exp") é a condição,
    "expT" é a expressão do "then" e "expE" é a expressão do "else". A semântica (comportamento)
    pretendido é o seguinte: compare o valor resultante da avaliação de "exp" com 0.
@@ -103,21 +109,21 @@ instance Show Valor where
   show (ValorFun f) = show f
 --(\(Ident x) -> x) nf
 
-type RContext = [(Ident,Valor)]
+type RContext = [(Decl,Valor)] -- Contexto de execução: associa Decl (tipo e id) a Valor
 
 lookup :: RContext -> Ident -> Valor
-lookup ((i,v):cs) s
+lookup (((Dec t i),v):cs) s
    | i == s = v
    | otherwise = lookup cs s
-
-update :: RContext -> Ident -> Valor -> RContext
+  
+update :: RContext -> Decl -> Valor -> RContext -- 
 update [] s v = [(s,v)]
-update ((i,v):cs) s nv
-  | i == s = (i,nv):cs
-  | otherwise = (i,v) : update cs s nv
+update ((d,v):cs) s nv
+  | d == s = (d,nv):cs                 -- Se encontrar o decl (tp,id), atualiza o valor
+  | otherwise = (d,v) : update cs s nv -- Senão, continua procurando
 
 
 updatecF :: RContext -> [Function] -> RContext
 updatecF c [] = c
-updatecF c (f:fs) = updatecF (update c (getName f) (ValorFun f)) fs
+updatecF c (f:fs) = updatecF (update c (getDecl f) (ValorFun f)) fs -- Percorre a lista de funções, atualizando o contexto com cada função e seu valor (ValorFun)
 
