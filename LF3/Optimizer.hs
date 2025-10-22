e7cce9689b06c7fa51a51faffa8d0505e30afb55
Willyan <willyanmarquesmelo@gmail.com>
2025-10-22 13:54:02 -0300

    modified:   LF3/Optimizer.hs

--- LF3/Optimizer.hs
+++ LF3/Optimizer.hs
@@ -13,40 +13,40 @@ optimizeF (Fun tR id decls exp) = Fun tR id decls (optimizeE exp)
 
 optimizeE :: Exp -> Exp
 optimizeE exp  = case exp of
-  EStr str -> EStr str
-  ETrue    -> ETrue
-  EFalse   -> EFalse
-  EInt n   -> EInt n
-  EVar id  -> EVar id
+    EStr str -> EStr str
+    ETrue    -> ETrue
+    EFalse   -> EFalse
+    EInt n   -> EInt n
+    EVar id  -> EVar id
 
-  ECon exp0 exp -> combOptimize ECon exp0 exp
-  EAdd exp0 exp -> combOptimize EAdd exp0 exp
-  ESub exp0 exp -> combOptimize ESub exp0 exp
-  EMul exp0 exp -> combOptimize EMul exp0 exp
-  EDiv exp0 exp -> combOptimize EDiv exp0 exp
-  EOr  exp0 exp -> combOptimize EOr exp0 exp
-  EAnd exp0 exp -> combOptimize EAnd exp0 exp
+    ECon exp0 exp -> combOptimize ECon exp0 exp
+    EAdd exp0 exp -> combOptimize EAdd exp0 exp
+    ESub exp0 exp -> combOptimize ESub exp0 exp
+    EMul exp0 exp -> combOptimize EMul exp0 exp
+    EDiv exp0 exp -> combOptimize EDiv exp0 exp
+    EOr  exp0 exp -> combOptimize EOr exp0 exp
+    EAnd exp0 exp -> combOptimize EAnd exp0 exp
 
-  ENot exp ->  let  optExp  = optimizeE  exp
-                    optENot = ENot optExp in
-                    if (isLiteral optExp)
-                      then  wrapValueExpression (eval [] optENot )
-                      else  optENot
+    ENot exp ->  let  optExp  = optimizeE  exp
+                      optENot = ENot optExp in
+                      if (isLiteral optExp)
+                        then  wrapValueExpression (eval [] optENot )
+                        else  optENot
 
-  -- TODO: substitua os 3 undefineds abaixo pelo retorno apropriado   
-  ECall exp lexp   -> ECall (optimizeE exp) (map optimizeE lexp)
-  ELambda params exp -> ELambda params (optimizeE exp)
-  EComp exp1 exp2 -> EComp (optimizeE exp1) (optimizeE exp2)
+    -- TODO: substitua os 3 undefineds abaixo pelo retorno apropriado   
+    ECall exp lexp   -> ECall (optimizeE exp) (map optimizeE lexp)
+    ELambda params exp -> ELambda params (optimizeE exp)
+    EComp exp1 exp2 -> EComp (optimizeE exp1) (optimizeE exp2)
 
-  EIf exp expT expE -> let  optExp  = optimizeE exp
-                            optThen = optimizeE expT
-                            optElse = optimizeE expE
-                            optEIf  = EIf optExp optThen optElse in
-                              case optExp of
-                                EInt vExpIf -> if (vExpIf == 0)
-                                                  then optElse
-                                                  else optThen
-                                _              -> optEIf
+    EIf exp expT expE -> let  optExp  = optimizeE exp
+                              optThen = optimizeE expT
+                              optElse = optimizeE expE
+                              optEIf  = EIf optExp optThen optElse in
+                                case optExp of
+                                  EInt vExpIf -> if (vExpIf == 0)
+                                                    then optElse
+                                                    else optThen
+                                  _              -> optEIf
 
 isLiteral :: Exp -> Bool
 isLiteral exp = case exp of
