1ceb34ffdc776add27ec4dea0af490646513f07d
Willyan Marques <100526142+marqueswill@users.noreply.github.com>
2025-11-26 11:34:24 -0300

    aaaa

---
+++ LF4/Interpreter2.hs
@@ -0,0 +1,249 @@
+module Interpreter where
+
+import AbsLF
+import AbsLF (Ident)
+import AbsLFAux
+import Control.Monad.State (MonadState (get), StateT, lift, runStateT)
+import Prelude hiding (lookup)
+
+executeP :: Program -> (Valor, Enviroment)
+executeP (Prog fs) =
+  case runStateT (eval (expMain fs)) initialEnv of
+    Just (val, finalEnv) -> (val, finalEnv)
+    Nothing -> error "Runtime Error"
+  where
+    initialContext = updatecF [] fs
+    initialMemory = []
+    initialEnv = (initialContext, initialMemory)
+
+    expMain (f : xs)
+      | getName f == Ident "main" = getExp f
+      | otherwise = expMain xs
+
+type Eval a = StateT Enviroment Maybe a
+
+eval :: Exp -> Eval Valor
+eval x = case x of
+  EStr str -> return (ValorStr str)
+  ETrue -> return (ValorBool True)
+  EFalse -> return (ValorBool False)
+  EInt n -> return (ValorInt n)
+  EVar id -> do
+    (ctx, _) <- get
+    return (lookup ctx id)
+  ECon exp0 exp -> do
+    v1 <- eval exp0
+    v2 <- eval exp
+    return (ValorStr (s v1 ++ s v2))
+  EAdd exp0 exp -> do
+    v1 <- eval exp0
+    v2 <- eval exp
+    return (ValorInt (i v1 + i v2))
+  ESub exp0 exp -> do
+    v1 <- eval exp0
+    v2 <- eval exp
+    return (ValorInt (i v1 - i v2))
+  EMul exp0 exp -> do
+    v1 <- eval exp0
+    v2 <- eval exp
+    return (ValorInt (i v1 * i v2))
+  EDiv exp0 exp -> do
+    v1 <- eval exp0
+    v2 <- eval exp
+    if i v2 == 0
+      then lift Nothing -- Divisão por 0
+      else return (ValorInt (i v1 `div` i v2))
+  EOr exp0 exp -> do
+    v1 <- eval exp0
+    v2 <- eval exp
+    return (ValorBool (b v1 || b v2))
+  EAnd exp0 exp -> do
+    v1 <- eval exp0
+    v2 <- eval exp
+    return (ValorBool (b v1 && b v2))
+  ENot exp -> do
+    v1 <- eval exp
+    return (ValorBool (not (b v1)))
+  EIf exp expT expE -> case eval exp of
+    (ValorInt v) ->
+      if v /= 0
+        then eval expT
+        else eval expE
+
+  ECall id lexp -> do
+    -- A. Evaluate arguments in current scope
+    argValues <- mapM eval lexp
+    (currentCtx, currentMem) <- get
+    case lookupECallLog currentMem id argValues of
+      Just val -> return val -- Cache Hit!
+      Nothing -> do
+        let (ValorFun funDef) = lookup currentCtx id
+        let parameters = getParams funDef
+
+        -- Contexto para chamada
+        let contextFunctions = filter (\(_, v) -> case v of ValorFun _ -> True; _ -> False) currentCtx
+        let paramBindings = zip (map Decl parameters) argValues
+        let newContext = paramBindings ++ contextFunctions
+
+        
+        put (newContext, currentMem)
+        resultVal <- eval (getExp funDef)
+
+        (_, memAfterBody) <- get
+
+        let finalMem = updateECallMem memAfterBody id argValues resultVal
+
+        put (currentCtx, finalMem)
+        
+        return resultVal
+      
+  ECall id lexp -> case lookupECallLog mem id argValues of -- Procura pelo resultado no cache
+    Just valor -> (valor, env) -- Se encontrar o resultado na memória, retorna o valor que tava guardado
+    Nothing ->
+      let (resultVal, (ctxFinal, memFinal)) = eval ecallEnv (getExp funDef) -- Senão, faço a chamada da função para os args e retorna
+          newMem = updateECallMem memFinal id argValues resultVal -- Atualiza a memória com o novo retorna
+       in (resultVal, (context, newMem)) -- Retorna o valor calculado, a
+    where
+      (ValorFun funDef) = lookup context id
+      parameters = getParams funDef
+      paramBindings = zip parameters (map (fst . eval env) lexp)
+
+      contextFunctions =
+        filter
+          ( \(i, v) -> case v of
+              ValorFun _ -> True
+              _ -> False
+          )
+          context
+      ecallContext = paramBindings ++ contextFunctions
+
+      -- TODO: fold para propagar mudança no env entre eval dos args
+      -- argValues = map (fst . eval env) lexp
+      (argValues, envAfterArgs) =
+        foldl
+          ( \(vals, currentEnv) exp ->
+              -- Função lambda para dar eval com o env atual
+              let (val, nextEnv) = eval currentEnv exp
+               in (vals ++ [val], nextEnv) -- Concatena o valor resultante, propaga o novo env para o proximo eval
+          )
+          ([], env) -- Inicialmente temos uma lista de valores vazia, e o env inicial
+          lexp -- Lista de expressões da função
+      memWithArgs = snd envAfterArgs
+      ecallEnv = (ecallContext, memWithArgs)
+
+{-
+ECall id lexp -> case lookupECallLog mem id argValues of -- Procura pelo resultado no cache
+
+  Just valor -> (valor, env) -- Se encontrar o resultado na memória, retorna o valor que tava guardado
+  Nothing ->
+    let (resultVal, (ctxFinal, memFinal)) = eval ecallEnv (getExp funDef) -- Senão, faço a chamada da função para os args e retorna
+        newMem = updateECallMem memFinal id argValues resultVal -- Atualiza a memória com o novo retorna
+     in (resultVal, (context, newMem)) -- Retorna o valor calculado, a
+  where
+    (ValorFun funDef) = lookup context id
+    parameters = getParams funDef
+    paramBindings = zip parameters (map (fst . eval env) lexp)
+
+    contextFunctions =
+      filter
+        ( \(i, v) -> case v of
+            ValorFun _ -> True
+            _ -> False
+        )
+        context
+    ecallContext = paramBindings ++ contextFunctions
+
+    -- TODO: fold para propagar mudança no env entre eval dos args
+    -- argValues = map (fst . eval env) lexp
+    (argValues, envAfterArgs) =
+      foldl
+        ( \(vals, currentEnv) exp ->
+            -- Função lambda para dar eval com o env atual
+            let (val, nextEnv) = eval currentEnv exp
+             in (vals ++ [val], nextEnv) -- Concatena o valor resultante, propaga o novo env para o proximo eval
+        )
+        ([], env) -- Inicialmente temos uma lista de valores vazia, e o env inicial
+        lexp -- Lista de expressões da função
+    memWithArgs = snd envAfterArgs
+    ecallEnv = (ecallContext, memWithArgs)
+-}
+
+{- Se eu fosse fazer memorização de resultados de chamada, eu precisaria:
+  1) Armazenar os resultados após cada chamada
+  2) O que eu preciso armazenar -> id da função, parâmetros informados, resultado da chamada
+    -- data Function = Fun Type Ident [Decl] Exp
+    -- padrão do context: [Ident, [[Exp], Exp]]
+  3) Antes de fazer a chamada, eu primeiro:
+    a) verifico se a função já foi chamada antes
+    b) comparo as lista de parâmetro com as listas já armazenadas
+    c) se as listas forem iguais, eu retorno o resultado que foi guardado
+      Caso contrário, eu faço o ecall normalmente e armazeno o novo resultado.
+-}
+
+-- *** @dica: nao altere o todo o codigo abaixo a partir daqui
+
+data Valor
+  = ValorInt {i :: Integer}
+  | ValorFun {f :: Function}
+  | ValorStr {s :: String}
+  | ValorBool {b :: Bool}
+  deriving (Eq)
+
+instance Show Valor where
+  show (ValorBool b) = show b
+  show (ValorInt i) = show i
+  show (ValorStr s) = s
+  show (ValorFun f) = show f
+
+instance Eq Decl where
+  (Dec t1 i1) == (Dec t2 i2) = i1 == i2
+
+type RContext = [(Decl, Valor)] -- Contexto de execução: associa Decl (tipo e id) a Valor
+
+type ECallResults = [([Valor], Valor)] -- Lista de parâmetros e o resultado da chamada
+
+type ECallLog = (Ident, ECallResults) -- Id do resultado de cada chamada para um dado args
+
+type ECallMem = [ECallLog]
+
+type Enviroment = (RContext, ECallMem)
+
+lookupECallLog :: ECallMem -> Ident -> [Valor] -> Maybe Valor
+lookupECallLog [] _ _ = Nothing
+lookupECallLog ((idLog, results) : logs) id args
+  | id == idLog = getECallResult results args
+  | otherwise = lookupECallLog logs id args
+
+getECallResult :: ECallResults -> [Valor] -> Maybe Valor
+getECallResult [] _ = Nothing
+getECallResult ((args, result) : logs) args'
+  | args == args' = Just result
+  | otherwise = getECallResult logs args'
+
+-- Primeiro eu encontro os logs para a função
+-- Em seguida eu chamo a função updateResults para atualizar o log
+updateECallMem :: ECallMem -> Ident -> [Valor] -> Valor -> ECallMem
+updateECallMem [] id args res = [(id, [(args, res)])]
+updateECallMem ((idLog, results) : logs) id args res
+  | id == idLog = (id, updateResults results args res) : logs
+  | otherwise = (idLog, results) : updateECallMem logs id args res
+
+updateResults :: ECallResults -> [Valor] -> Valor -> ECallResults
+updateResults [] args' res' = [(args', res')]
+updateResults ((args, res) : xs) args' res'
+  | args == args' = (args, res) : xs
+  | otherwise = (args, res) : updateResults xs args' res'
+
+lookup :: RContext -> Ident -> Valor
+lookup ((Dec t i, v) : cs) s
+  | i == s = v
+  | otherwise = lookup cs s
+
+update :: RContext -> Decl -> Valor -> RContext --
+update [] s v = [(s, v)]
+update ((d, v) : cs) s nv
+  | d == s = (d, nv) : cs -- Se encontrar o decl (tp,id), atualiza o valor
+  | otherwise = (d, v) : update cs s nv -- Senão, continua procurando
+
+updatecF :: RContext -> [Function] -> RContext
+updatecF = foldl (\c f -> update c (getDecl f) (ValorFun f)) -- Percorre a lista de funções, atualizando o contexto com cada função e seu valor (ValorFun)
